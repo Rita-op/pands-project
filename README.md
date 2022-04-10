1bb6f68ea656a452a94223c0d7931ab0f45c0d19
Rita Ortega <ritaortega@MacBook-Air-de-Rita.local>
2022-04-10 01:17:25 +0100

    Added the iris_data.csv and the analysis.py files

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # pands-project
+
+## This repository contains the solution to the project of the module: " Programming and Scripting"
+
+-----------
+
+Database: http://archive.ics.uci.edu/ml/datasets/Iris 
+
---
+++ analysis.py
@@ -0,0 +1,17 @@
+# SECTION 1: Outputs a summary of each variable to a single text file
+
+
+# SECTION 2: Saves a histogram of each variable to png files
+
+
+# SECTION 3: Outputs a scatter plot of each pair of variables
+
+
+# SECTION 4: Additional analysis that I think is appropriate for this case
+
+
+
+
+
+
+
